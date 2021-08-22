5c0cd21ed3d04837388113abf8a966a07d19778e
Unai Abrisketa <u.abrisketa@epages.com>
2021-08-22 15:24:12 +0200

    Initial commit

---
+++ lib/liquid/tag/attribute_parser.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "attribute_parser/version"
+
+module Liquid
+  module Tag
+    module AttributeParser
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/liquid/tag/attribute_parser/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Liquid
+  module Tag
+    module AttributeParser
+      VERSION = "0.1.0"
+    end
+  end
+end
